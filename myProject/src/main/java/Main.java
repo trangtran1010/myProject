0836e2b1be9ac105db39e9c5643cb242a54e446b
trangtran1010 <142396603+trangtran1010@users.noreply.github.com>
2023-08-22 14:55:14 +0700

    Add files via upload

--- myProject/src/main/java/Main.java
+++ myProject/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        printJSON printJSON = new printJSON();
-        printJSON.commentLine();
+        printJSON printJson = new printJSON();
+        printJson.commentLine();
     }
 }
